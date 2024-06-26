f883080c5b6d595976fbcf5f614307222e836cae
Alex Fischer <alexander.fischer3@gmail.com>
2024-06-26 14:43:20 -0600

    remove setup.sh and add readme

---
+++ README.md
@@ -0,0 +1,2 @@
+# Find sinkholes automatically
+This script automatically detects sinkholes using LIDAR elevation data from the USGS. Instructions for use are at [caves.science](https://caves.science).
--- setup.sh
+++
@@ -1,9 +0,0 @@
-sudo add-apt-repository ppa:ubuntugis/ppa
-sudo apt-get update
-sudo apt install libgdal-dev
-
-export CPLUS_INCLUDE_PATH=/usr/include/gdal
-
-export C_INCLUDE_PATH=/usr/include/gdal
-
-pip install -r requirements.txt
