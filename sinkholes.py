4208d7011e6ebdf9b046839bc15f7b93253ca8c7
Alex Fischer <alexander.fischer3@gmail.com>
2024-06-28 12:12:52 -0600

    add azimuth and altitude angles for hillshade map as config options

--- sinkholes.py
+++ sinkholes.py
@@ -48,7 +48,7 @@ def process_geotiff(geotiff_input_filename, geotiff_output_filename, sinkholes_o
 
     if output_geotiff:
         # make hillshade map with sinkholes highlighted by depth
-        hillshade = es.hillshade(elevation, azimuth=315, altitude=30)
+        hillshade = es.hillshade(elevation, azimuth=config['hillshade_azimuth'], altitude=config['hillshade_altitude'])
         img = np.zeros(shape=(3, hillshade.shape[0], hillshade.shape[1]), dtype=np.uint8)
         for channel in range(3):
             img[channel, :, :] = hillshade # img has all 3 channels equal to hillshade map (so img is black and white hillshade map)
@@ -201,7 +201,9 @@ def default_config():
         'max_points_per_file': -1, # if there are more than this number of points, split them up into multiple files (useful because e.g. gaiagps can't handle more than 1000 points per file). Set it to -1 for no max
         "pin_colormap": "gist_rainbow", # matplotlib colormap name to use for pin color. Not recommended to be the same as map_depth_colormap because pin_color_colormap will be used with a log scale, unlike map_depth_colormap
         "map_colormap": "inferno_r", # matplotlib colormap name to use for depth colorcoding in output map. Not recommended to be the same as pin_color_colormap because pin_color_colormap will be used with a log scale, unlike map_depth_colormap
-        "verbose": True
+        "hillshade_azimuth": 315, # Sun angle for hillshade map. See desktop.arcgis.com/en/arcmap/latest/tools/spatial-analyst-toolbox/ for explanation
+        "hillshade_altitude": 45, # sun angle for hillshade map
+        "verbose": True,
     }
 
 parser = argparse.ArgumentParser(prog="Find Sinkholes", description="Automatically find sinkholes using 1m DEMs from USGS")
@@ -234,43 +236,55 @@ if 'config' in args and args.config != None:
             if 'min_depth' in config_json:
                 min_depth = config_json['min_depth']
                 if type(min_depth) in (int, float) and min_depth >= 0:
-                    config['min_depth'] = config_json['min_depth'] * unit_conversion_constant
+                    config['min_depth'] = min_depth * unit_conversion_constant
                 else:
-                    print(f'Option min_depth in config "{str(min_depth)}" file is invalid: must be nonnegative number. Defaulting to {config["min_depth"]}.')
+                    print(f'Option min_depth in config file "{str(min_depth)}" is invalid: must be nonnegative number. Defaulting to {config["min_depth"]}.')
             if 'max_dimension' in config_json:
                 max_dimension = config_json['max_dimension']
                 if type(max_dimension) in (int, float) and max_dimension > 0:
-                    config['max_dimension'] = config_json['max_dimension'] * unit_conversion_constant
+                    config['max_dimension'] = max_dimension * unit_conversion_constant
                 else:
-                    print(f'Option max_dimension in config "{str(max_dimension)}" file is invalid: must be positive number. Defaulting to {config["max_dimension"]}.')
+                    print(f'Option max_dimension in config file "{str(max_dimension)}" is invalid: must be positive number. Defaulting to {config["max_dimension"]}.')
             if 'min_depth_for_colormap' in config_json:
                 min_depth_for_colormap = config_json['min_depth_for_colormap']
                 if type(min_depth_for_colormap) in (int, float) and min_depth_for_colormap > 0:
-                    config['min_depth_for_colormap'] = config_json['min_depth_for_colormap'] * unit_conversion_constant
+                    config['min_depth_for_colormap'] = min_depth_for_colormap * unit_conversion_constant
                 else:
-                    print(f'Option min_depth_for_colormap in config "{str(min_depth_for_colormap)}" file is invalid: must be positive number. Defaulting to {config["min_depth_for_colormap"]}.')
+                    print(f'Option min_depth_for_colormap in config file "{str(min_depth_for_colormap)}" is invalid: must be positive number. Defaulting to {config["min_depth_for_colormap"]}.')
             if 'max_depth_for_colormap' in config_json:
                 max_depth_for_colormap = config_json['max_depth_for_colormap']
                 if type(max_depth_for_colormap) in (int, float) and max_depth_for_colormap > 0:
-                    config['max_depth_for_colormap'] = config_json['max_depth_for_colormap'] * unit_conversion_constant
+                    config['max_depth_for_colormap'] = max_depth_for_colormap * unit_conversion_constant
                 else:
-                    print(f'Option max_depth_for_colormap in config "{str(max_depth_for_colormap)}" file is invalid: must be positive number. Defaulting to {config["max_depth_for_colormap"]}.')
+                    print(f'Option max_depth_for_colormap in config file "{str(max_depth_for_colormap)}" is invalid: must be positive number. Defaulting to {config["max_depth_for_colormap"]}.')
             if 'max_points_per_file' in config_json:
                 max_points_per_file = config_json['max_points_per_file']
                 if type(max_points_per_file) == int:
-                    config['max_points_per_file'] = config_json['max_points_per_file']
+                    config['max_points_per_file'] = max_points_per_file
                 else:
-                    print(f'Option max_points_per_file in config "{str(max_points_per_file)}" file is invalid: must be an integer. Defaulting to {config["max_points_per_file"]}.')
+                    print(f'Option max_points_per_file in config file "{str(max_points_per_file)}" is invalid: must be an integer. Defaulting to {config["max_points_per_file"]}.')
             if 'pin_colormap' in config_json and type(config_json['pin_colormap']) is str:
                 config['pin_colormap'] = config_json['pin_colormap']
             if 'map_colormap' in config_json and type(config_json['map_colormap']) is str:
                 config['map_colormap'] = config_json['map_colormap']
+            if 'hillshade_azimuth' in config_json:
+                hillshade_azimuth = config['hillshade_azimuth']
+                if type(hillshade_azimuth) in (int, float):
+                    config['hillshade_azimuth'] = hillshade_azimuth
+                else:
+                    print(f'Option hillshade_azimuth in config file "{str(hillshade_azimuth)}" is invalid: must be a number. Defaulting to {config["hillshade_azimuth"]}.')
+            if 'hillshade_altitude' in config_json:
+                hillshade_altitude = config['hillshade_altitude']
+                if type(hillshade_altitude) in (int, float):
+                    config['hillshade_altitude'] = hillshade_altitude
+                else:
+                    print(f'Option hillshade_altitude in config file "{str(hillshade_altitude)}" is invalid: must be a number. Defaulting to {config["hillshade_altitude"]}.')
             if 'verbose' in config_json:
                 verbose = config['verbose']
                 if type(verbose) == bool:
                     config['verbose'] = config_json['verbose']
                 else:
-                    print(f'Option verbose in config "{str(verbose)}" file is invalid: must be a boolean. Defaulting to {config["verbose"]}.')
+                    print(f'Option verbose in config file "{str(verbose)}" is invalid: must be a boolean. Defaulting to {config["verbose"]}.')
     except FileNotFoundError:
         print(f'No config file "{args.config}" found. Using default values.')
         config = default_config()
